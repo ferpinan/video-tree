7a5f10e5c3131702a3c0e13c189078afd360e6e6
andonifernandez <andonifernandez@paradigmadigital.com>
2022-03-17 13:31:53 +0100

    Playbackrate buttons

--- src/VideoPlayer/VideoPlayer.js
+++ src/VideoPlayer/VideoPlayer.js
@@ -1,8 +1,28 @@
 import PropTypes from 'prop-types';
 import BackButton from "../BackButton/BackButton";
+import React, {useState} from "react";
 
 function VideoPlayer(props) {
     const {onClickBackButton, src, isVisible} = {...props};
+    const [currentPlaybackRate, setCurrentPlaybackRate] = useState(1);
+    const videoRef = React.createRef();
+
+    const onClickSlowerButton = () => {
+        if(videoRef.current.playbackRate>0.1) {
+            videoRef.current.playbackRate = videoRef.current.playbackRate - 0.1;
+            setCurrentPlaybackRate(videoRef.current.playbackRate);
+        }
+    }
+    const onClickFasterButton = () => {
+        if(videoRef.current.playbackRate<3) {
+            videoRef.current.playbackRate = videoRef.current.playbackRate + 0.1;
+            setCurrentPlaybackRate(videoRef.current.playbackRate);
+        }
+    }
+    const onClickResetButton = () => {
+        videoRef.current.playbackRate = 1;
+        setCurrentPlaybackRate(videoRef.current.playbackRate);
+    }
 
     if(!isVisible){
         return "";
@@ -10,7 +30,11 @@ function VideoPlayer(props) {
 
     return (
         <>
-            <video controls style={{"width": "50%"}} src={src} />
+            <video controls style={{"width": "50%"}} src={src} ref={videoRef} />
+            <button onClick={onClickSlowerButton}>Slower</button>
+            <button onClick={onClickFasterButton}>Faster</button>
+            <button onClick={onClickResetButton}>Reset Playback Rate</button>
+            <p>Current playback rate: {currentPlaybackRate}</p>
             <BackButton onClick={onClickBackButton} />
         </>
     );
